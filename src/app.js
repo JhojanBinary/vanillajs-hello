40c000c509f192204f143269ac5f5583c800817f
JhojanBinary <102551448+JhojanBinary@users.noreply.github.com>
2024-02-16 23:57:28 +0000

    excuses

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener('DOMContentLoaded', function() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const excuseParagraph = document.getElementById('excuse');
+  const generateExcuseButton = document.getElementById('generate-excuse');
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  generateExcuseButton.addEventListener('click', function() {
+    let randomWhoIndex = Math.floor(Math.random() * who.length);
+    let randomActionIndex = Math.floor(Math.random() * action.length);
+    let randomWhatIndex = Math.floor(Math.random() * what.length);
+    let randomWhenIndex = Math.floor(Math.random() * when.length);
+
+    let randomExcuse = who[randomWhoIndex] + ' ' + action[randomActionIndex] + ' ' + what[randomWhatIndex] + ' ' + when[randomWhenIndex];
+
+    excuseParagraph.textContent = randomExcuse;
+  });
+});
